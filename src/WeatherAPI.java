40a05ab6052be91d3991cadaf419bbedf81d1fd8
Stefanie <s.j.pfleiderer@web.de>
2021-06-05 17:57:04 +0200

    Get "from" time attribute and temperature

--- src/WeatherAPI.java
+++ src/WeatherAPI.java
@@ -1,4 +1,7 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,7 +16,20 @@ public class WeatherAPI {
         DocumentBuilder documentBuilder = factory.newDocumentBuilder();
 
         Document document = documentBuilder.parse(uri);
+        NodeList times = document.getElementsByTagName("time");
 
+        for (int x = 0; x < times.getLength(); x++) {
+            Node time = times.item(x);
+            NamedNodeMap timeAttributes = time.getAttributes();
 
+            String timestamp = timeAttributes.getNamedItem("from").getNodeValue();
+            NodeList children = time.getChildNodes();
+            for (int y = 0; y < children.getLength(); y++) {
+                Node child = children.item(y);
+                if (child.getNodeName().equals("temperature")) {
+                    String temperature = child.getAttributes().getNamedItem("value").getNodeValue();
+                }
+            }
+        }
     }
 }
